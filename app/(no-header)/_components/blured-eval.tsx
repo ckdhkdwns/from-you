93bb81e6be09e8725a0535ab7d2cc7d5fb13b53c
차왕준 <ckdhdkwns@gmail.com>
2025-03-18 00:26:17 +0900

    fix: 로그인, 회원가입 input event 시 버벅임 수정
    - svg를 바로 컴포넌트로 사용하는 것이 아니라 Image로 사용하여 리렌더링 방지

--- app/(no-header)/_components/blured-eval.tsx
+++ app/(no-header)/_components/blured-eval.tsx
@@ -1,58 +1,16 @@
 'use client';
 
+import Image from 'next/image';
+
 export default function BluredEval() {
     return (
-        <svg
-            width={'100%'}
-            height={'100%'}
-            viewBox="0 0 1440 836"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-        >
-            <g filter="url(#filter0_f_74_857)">
-                <ellipse
-                    cx={720}
-                    cy={38}
-                    rx={743}
-                    ry={598}
-                    fill="url(#paint0_linear_74_857)"
-                    fillOpacity="0.2"
-                />
-            </g>
-            <defs>
-                <filter
-                    id="filter0_f_74_857"
-                    x={-223}
-                    y={-760}
-                    width={1886}
-                    height={1596}
-                    filterUnits="userSpaceOnUse"
-                    colorInterpolationFilters="sRGB"
-                >
-                    <feFlood floodOpacity={0} result="BackgroundImageFix" />
-                    <feBlend
-                        mode="normal"
-                        in="SourceGraphic"
-                        in2="BackgroundImageFix"
-                        result="shape"
-                    />
-                    <feGaussianBlur stdDeviation={100} result="effect1_foregroundBlur_74_857" />
-                </filter>
-                <linearGradient
-                    id="paint0_linear_74_857"
-                    x1="231.027"
-                    y1="-377.661"
-                    x2="203.721"
-                    y2="833.009"
-                    gradientUnits="userSpaceOnUse"
-                >
-                    <stop stopColor="#FFCDE0" />
-                    <stop offset="0.215" stopColor="#F8D3D5" />
-                    <stop offset="0.415" stopColor="#FFC8F8" />
-                    <stop offset={1} stopColor="#F8D3D5" />
-                </linearGradient>
-            </defs>
-        </svg>
+        <Image
+            src="/blured-eval.svg"
+            alt="Blured evaluation background"
+            width={1440}
+            height={836}
+            className="w-full h-full"
+            priority
+        />
     );
 }
